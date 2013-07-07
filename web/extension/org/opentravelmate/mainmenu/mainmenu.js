342dc6ef2217d403e17e57a088f0e702964af014
Marc Plouhinec <m_plouhinec@yahoo.fr>
2013-07-07 16:13:06 +0200

    Improve the main menu.

--- web/extension/org/opentravelmate/mainmenu/mainmenu.js
+++ web/extension/org/opentravelmate/mainmenu/mainmenu.js
@@ -4,11 +4,35 @@
  * @author marc.plouhinec@gmail.com (Marc Plouhinec)
  */
 
-define(function() {
+define(['jquery'], function($) {
+	
+	/**
+	 * Add a button to the panel.
+	 * 
+	 * @param {String} title
+	 * @param {String} tooltip
+	 * @param {String} imageUrl
+	 */
+	function addButton(title, tooltip, imageUrl) {
+		var button = document.createElement('button');
+		button.id = 'button-' + title;
+		button.setAttribute('title', tooltip);
+		var buttonImage = document.createElement('img');
+		buttonImage.src = imageUrl;
+		buttonImage.setAttribute('alt', title);
+		button.appendChild(buttonImage);
+		var buttonSize = $('#button-panel').height();
+		button.style.width = buttonSize + 'px';
+		button.style.height = buttonSize + 'px';
+		
+		document.getElementById('button-panel').appendChild(button);
+	};
+	
 	/**
 	 * Main menu entry point.
 	 */
 	return function mainmenu() {
-		console.log('hello from mainmenu');
+		// Display the more button
+		addButton('More', 'More', 'image/ic_btn_more.png');
 	};
 });
