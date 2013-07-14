f7808abbb15b8fe5ed809a52ff3c33f7b3397f2e
Marc Plouhinec <m_plouhinec@yahoo.fr>
2013-07-14 10:59:06 +0200

    Remove the android proof of concept.

--- android/widget/map/gen/org/opentravelmate/widget/map/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.opentravelmate.widget.map;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- android/widget/webview/gen/org/opentravelmate/widget/webview/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.opentravelmate.widget.webview;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
