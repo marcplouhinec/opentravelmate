dee57ceaf4482eaf15da9d100cb0f76984d247b7
Marc Plouhinec <m_plouhinec@yahoo.fr>
2013-08-29 23:04:45 +0200

    Improve the MarkerRTree.js.

--- web/native/widget/map/MarkerRTree.js
+++ web/native/widget/map/MarkerRTree.js
@@ -4,7 +4,7 @@
  * @author marc.plouhinec@gmail.com (Marc Plouhinec)
  */
 
-define(['./projectionUtils'], function(projectionUtils) {
+define(['underscore', './projectionUtils'], function(_, projectionUtils) {
     'use strict';
 
     /**
@@ -46,10 +46,18 @@ define(['./projectionUtils'], function(projectionUtils) {
         /**
          * Contains all the markers grouped by tiles.
          *
-         * @type {Object.<Number, Object.<String, Array>>}
+         * @type {Object.<Number, Array<{marker: Object, x: Number, y: Number}>>}
          * @private
          */
-        this._markersByTileIdByZoom = {};
+        this._markersByTileId = {};
+
+        /**
+         * Contains all the tiles grouped by marker.
+         *
+         * @type {Object.<String, Array.<String>>}
+         * @private
+         */
+        this._tileIdsByMarkerId = {};
     };
 
     /**
@@ -68,12 +76,6 @@ define(['./projectionUtils'], function(projectionUtils) {
                 y: projectionUtils.latToTileY(2, marker.position.lat)
             };
 
-            var markersByTileId = this._markersByTileIdByZoom[zoom];
-            if (!markersByTileId) {
-                markersByTileId = {};
-                this._markersByTileIdByZoom[zoom] = markersByTileId;
-            }
-
             var markerCornerPositions = [
                 {
                     x: markerPositionXY.x - markerAnchor.x / 256,
@@ -91,13 +93,27 @@ define(['./projectionUtils'], function(projectionUtils) {
             ];
 
             for (var i = 0; i < 4; i += 1) {
-                var tileId = zoom + '_' + projectionUtils.tileYToLat(zoom, markerCornerPositions.y) + '_' + projectionUtils.tileXToLng(zoom, markerCornerPositions.x);
-                var markers = markersByTileId[tileId];
-                if (!markers) {
-                    markers = [];
-                    markersByTileId[tileId] = markers;
+                var tileId = zoom + '_' + projectionUtils.tileYToLat(zoom, markerCornerPositions[i].y) + '_' + projectionUtils.tileXToLng(zoom, markerCornerPositions[i].x);
+
+                // Add the marker to this._markersByTileId
+                var markerWithCoords = this._markersByTileId[tileId];
+                if (!markerWithCoords) {
+                    markerWithCoords = [];
+                    this._markersByTileId[tileId] = markerWithCoords;
                 }
-                markers.push(marker);
+                markerWithCoords.push({
+                    marker: marker,
+                    x: markerCornerPositions[i].x,
+                    y: markerCornerPositions[i].y
+                });
+
+                // Add the tile to this._tileIdsByMarkerId
+                var tileIds = this._tileIdsByMarkerId[String(marker.id)];
+                if (!tileIds) {
+                    tileIds = [];
+                    this._tileIdsByMarkerId[String(marker.id)] = tileIds;
+                }
+                tileIds.push(tileId);
             }
         }
     };
@@ -108,7 +124,27 @@ define(['./projectionUtils'], function(projectionUtils) {
      * @param marker
      */
     MarkerRTree.prototype.removeMarker = function(marker) {
-        // TODO
+        // Find the tileIds where the marker is located
+        var tileIds = this._tileIdsByMarkerId[String(marker.id)];
+        if (!tileIds) {
+            return;
+        }
+
+        // Delete the marker entry from this._tileIdsByMarkerId
+        delete this._tileIdsByMarkerId[String(marker.id)];
+
+        // Remove the marker from this._markersByTileId
+        for (var i = 0; i < tileIds.length; i += 1) {
+            var tileId = tileIds[i];
+            var markerWithCoords = this._markersByTileId[tileId];
+
+            this._markersByTileId[tileId] = _.filter(markerWithCoords, function (markerWithCoord) {
+                return markerWithCoord.marker.id !== marker.id;
+            });
+            if (this._markersByTileId[tileId].length === 0) {
+                delete this._markersByTileId[tileId];
+            }
+        }
     };
 
     /**
@@ -117,10 +153,31 @@ define(['./projectionUtils'], function(projectionUtils) {
      * @param {Number} zoom
      * @param {Number} x
      * @param {Number} y
-     * @return {{marker: Object, distance: Number}} Marker and distance in pixels
+     * @return {{marker: Object, distance: Number}|null} Marker and distance in pixels
      */
-    MarkerRTree.prototype.getNearestMarkerAndDistance = function(zoom, x, y) {
-        return null; // TODO
+    MarkerRTree.prototype.getNearestMarkerWithDistance = function(zoom, x, y) {
+        // Get all the markers in the given tile
+        var markerWithCoords = this._markersByTileId[Math.floor(zoom) + '_' + Math.floor(x) + '_' + Math.floor(y)];
+        if (!markerWithCoords || markerWithCoords.length === 0) {
+            return null;
+        }
+
+        // Find the nearest marker
+        var nearestDistance2 = markerWithCoords[0].x * x + markerWithCoords[0].y * y;
+        var nearestMarker = markerWithCoords[0].marker;
+
+        for (var i = 1; i < markerWithCoords.length; i += 1) {
+            var distance2 = markerWithCoords[i].x * x + markerWithCoords[i].y * y;
+            if (distance2 < nearestDistance2) {
+                nearestDistance2 = distance2;
+                nearestMarker = markerWithCoords[i].marker;
+            }
+        }
+
+        return {
+            marker: nearestMarker,
+            distance: Math.sqrt(nearestDistance2)
+        };
     };
 
     return MarkerRTree;
